a9eedf5d49239839f36bd3334919b1a36c77c5d0
jonpickering <jpickering@medifusion.co.uk>
2015-04-11 10:19:49 +0100

    update local path

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -26,8 +26,8 @@ namespace OrderMgt.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\U\\OodProject\\OrderMgt\\DataBase\\O" +
-            "rderMgt.accdb")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\CM0721\\OrderMgt\\DataBase\\OrderMg" +
+            "t.accdb")]
         public string ordersdb {
             get {
                 return ((string)(this["ordersdb"]));
